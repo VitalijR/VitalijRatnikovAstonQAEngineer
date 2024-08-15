89aa70eb77a9f9ab822de5df905caaaa3c192e83
Vitaliy <ratnikovvitalij11@gmail.com>
2024-08-15 21:14:00 +0300

    Initial commit

---
+++ src/main/java/Lesson6/Main.java
@@ -0,0 +1,9 @@
+package Lesson6;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee em1 = new Employee("Иван Фёдорович Крузенштерн", "Исследователь",
+                "seaexplorer@gmail.com", "+79001234567", 35000, 66);
+        em1.printInfo();
+    }
+}
